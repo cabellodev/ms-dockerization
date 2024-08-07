936c8f32a666c697facb1b27e8442d00aca42436
Sebastian Cabello <cabello.developer@gmail.com>
2024-08-07 17:48:21 -0400

    intento 7

--- src/main/java/com/example/ms_dockerizacion/Controller/MessageController.java
+++ src/main/java/com/example/ms_dockerizacion/Controller/MessageController.java
@@ -12,7 +12,7 @@ public class MessageController {
 
     @GetMapping(value="/message")
     public ResponseEntity<DtoMessage>get(){
-         DtoMessage message = DtoMessage.builder().message("ya tomo mucha fuerza el ci/cd ").autor("Sebastian Cabello").build();
+         DtoMessage message = DtoMessage.builder().message("estamos en onda").autor("Sebastian Cabello").build();
                  return ResponseEntity.ok(message);
     }
 }
